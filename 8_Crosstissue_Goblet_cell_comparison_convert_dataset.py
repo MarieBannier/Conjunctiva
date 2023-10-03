b53e1278ff692c55de9d865fe3843771a23a72ad
MarieBannier <59700748+MarieBannier@users.noreply.github.com>
2023-10-03 16:37:53 +0200

    Create 8_Crosstissue_Goblet_cell_comparison_convert_gut_dataset.py

---
+++ 8_Crosstissue_Goblet_cell_comparison_convert_dataset.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/home/user/python-libs')
+print (sys.path)
+
+import numpy as np
+import pandas as pd
+import scanpy as sc
+
+gut = sc.read("gut.h5ad") #epithelial cells only, from https://www.gutcellatlas.org/
+
+gut.obs.to_csv("gut_raw.csv")
